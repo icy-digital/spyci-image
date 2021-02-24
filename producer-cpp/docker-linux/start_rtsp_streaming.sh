a7b54b66e225e8035ac82ca121d0178384b34dba
Balou9 <emmanuel.owosekun@gmail.com>
2021-02-24 11:30:19 +0100

    cd: build docker images

--- producer-cpp/docker-linux/start_rtsp_streaming.sh
+++ producer-cpp/docker-linux/start_rtsp_streaming.sh
@@ -9,4 +9,4 @@ gst-launch-1.0 v4l2src do-timestamp=TRUE device=/dev/video0 ! videoconvert \
 ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 ! omxh264enc \
 control-rate=2 target-bitrate=512000 inline-header=FALSE periodicty-idr=20 ! \
 h264parse ! video/x-h264,stream-format=avc,alignment=au,width=640,height=480,framerate=30/1,profile=baseline ! \
-kvssink stream-name="YOURSTREAMNAME" access-key=YOURACCESSKEY secret-key=YOURSECRETKEY
+kvssink stream-name=$STREAM_NAME access-key=$AWS_ACCESS_KEY_ID secret-key=$AWS_SECRET_ACCESS_KEY
