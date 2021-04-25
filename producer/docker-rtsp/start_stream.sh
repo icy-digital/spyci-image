f70c633699c38d86429301ae5d0acffcf701dfa3
Balou9 <emmanuel.owosekun@gmail.com>
2021-04-25 04:29:04 +0200

    chore: uses x264enc props

--- producer/docker-rtsp/start_stream.sh
+++ producer/docker-rtsp/start_stream.sh
@@ -9,8 +9,7 @@ gst-launch-1.0 \
 v4l2src do-timestamp=TRUE device=/dev/video0 \
 ! videoconvert \
 ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 \
-! x264enc \
-! control-rate=2 target-bitrate=512000 inline-header=FALSE periodicty-idr=20 \
+! x264enc bframes=0 key-int-max=45 bitrate=500 \
 ! h264parse \
 ! video/x-h264,stream-format=avc,alignment=au,width=640,height=480,framerate=30/1,profile=baseline \
 ! kvssink stream-name=$STREAM_NAME access-key=$AWS_ACCESS_KEY_ID secret-key=$AWS_SECRET_ACCESS_KEY aws-region=$AWS_REGION
