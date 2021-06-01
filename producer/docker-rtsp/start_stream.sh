16b282a388aaacd0a782d8382bb4a76666a7bfd0
Balou9 <emmanuel.owosekun@gmail.com>
2021-06-01 18:26:39 +0200

    chore: removes entrypoint

--- producer/docker-rtsp/start_stream.sh
+++ producer/docker-rtsp/start_stream.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 # cd /opt/amazon-kinesis-video-streams-producer-sdk-cpp/build/
-STREAM_NAME=$1
-AWS_ACCESS_KEY_ID=$2
-AWS_SECRET_ACCESS_KEY=$3
-AWS_REGION=$4
-
 export GST_PLUGIN_PATH=/opt/amazon-kinesis-video-streams-producer-sdk-cpp/build
 export LD_LIBRARY_PATH=/opt/amazon-kinesis-video-streams-producer-sdk-cpp/open-source/local/lib
 
@@ -15,7 +10,10 @@ v4l2src do-timestamp=TRUE device=/dev/video0 \
 ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 \
 ! x264enc bframes=0 key-int-max=45 bitrate=500 \
 ! video/x-h264,stream-format=avc,alignment=au,width=640,height=480,framerate=30/1,profile=baseline \
-! kvssink stream-name=$STREAM_NAME access-key=$AWS_ACCESS_KEY_ID secret-key=$AWS_SECRET_ACCESS_KEY aws-region=$AWS_REGION
+! kvssink stream-name=$STREAM_NAME \
+access-key=$AWS_ACCESS_KEY_ID \
+secret-key=$AWS_SECRET_ACCESS_KEY \
+aws-region=$AWS_REGION
 
 # gst-launch-1.0 \ launches gstreamer1
 # v4l2src do-timestamp=TRUE device=/dev/video0 \
